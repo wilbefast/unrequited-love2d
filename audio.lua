4511d5669573d03e708d5b5c5f9d19ae4efe5d67
william <william@naturalpad.fr>
2014-05-24 04:24:15 +0200

    audio doesn't depend on a globally included useful

--- audio.lua
+++ audio.lua
@@ -12,6 +12,9 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 Lesser General Public License for more details.
 --]]
+
+local useful = require("unrequited/useful")
+
 local audio = { }
 
 -- loading
